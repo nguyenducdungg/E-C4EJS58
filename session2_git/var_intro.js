1a75872e49d66897952269df476ff79bf1c7f806
nguyenducdungg <nguyenducdung853@gmail.com>
2019-11-12 21:24:29 +0700

    session1

---
+++ session2_git/var_intro.js
@@ -0,0 +1,28 @@
+// alert ("Hi. Can I heplp you") ;
+
+let name ;
+var name1 ;
+const name2 = "Dũng";
+name = "Nguyễn";
+name1 = "Đức";
+
+
+// alert(name2);
+
+let x= 10 ;
+let y = 20 ;
+console.log(x);
+console.log(y);
+
+console.log( x+y);
+let z = x + y ;
+console.log(z*z);
+
+let ten = prompt("Tên bạn là gì ? ");
+console.log("xin chào " + ten );
+
+let tuoi = prompt("Tuổi của bạn") ;
+console.log("xin chào " + ten + " , bạn " + tuoi + " tuoi");
+
+let nam_sinh = prompt("Năm sinh của bạn ?")
+console.log("xin chào " + ten + " , bạn sinh năm " + nam_sinh  );
